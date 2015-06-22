b2b97de682db3b000343098b312ce32670a63eec
Ewan Dawson <ewan@lazygun.net>
2015-06-22 09:55:30 +0100

    Bug fixes.

--- src/main/java/net/lazygun/experiment/dropwizard/comsat/MyDropwizardApp.java
+++ src/main/java/net/lazygun/experiment/dropwizard/comsat/MyDropwizardApp.java
@@ -8,10 +8,13 @@ import co.paralleluniverse.fibers.dropwizard.FiberDBIFactory;
 import co.paralleluniverse.fibers.dropwizard.FiberHttpClientBuilder;
 import com.codahale.metrics.annotation.Timed;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.Optional;
 import com.google.common.io.Resources;
 import io.dropwizard.Configuration;
 import io.dropwizard.client.HttpClientConfiguration;
 import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.java8.Java8Bundle;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
@@ -24,22 +27,34 @@ import org.skife.jdbi.v2.sqlobject.SqlQuery;
 import org.skife.jdbi.v2.sqlobject.SqlUpdate;
 import org.skife.jdbi.v2.util.StringMapper;
 
+import javax.inject.Singleton;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import java.io.IOException;
-import java.util.Optional;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
+@Singleton
+@Path("/")
+@Produces(MediaType.APPLICATION_JSON)
 public class MyDropwizardApp extends FiberApplication<MyDropwizardApp.MyConfig> {
     private IDBI jdbi;
     private MyDAO dao;
     private HttpClient httpClient;
 
     public static void main(String[] args) throws Exception {
-        new MyDropwizardApp().run(new String[]{"server", Resources.getResource("server.yml").getPath()});
+        new MyDropwizardApp().run("server", Resources.getResource("server.yml").getPath());
+    }
+
+    @Override
+    public void initialize(Bootstrap<MyConfig> bootstrap) {
+        bootstrap.addBundle(new Java8Bundle());
     }
 
     @Override
@@ -98,10 +113,10 @@ public class MyDropwizardApp extends FiberApplication<MyDropwizardApp.MyConfig> 
 
     @Suspendable
     public interface MyDAO {
-        @SqlUpdate("create table if not exists something (id int primary key, name varchar(100)")
+        @SqlUpdate("create table if not exists something (id int primary key, name varchar(100))")
         void createSomethingTable();
 
-        @SqlUpdate("drop something table")
+        @SqlUpdate("drop table something")
         void dropSomethingTable();
 
         @SqlUpdate("insert into something (id, name) values (:id, :name)")
@@ -117,44 +132,50 @@ public class MyDropwizardApp extends FiberApplication<MyDropwizardApp.MyConfig> 
     @Timed
     public Saying get(@QueryParam("name") Optional<String> name,
                       @QueryParam("sleep") Optional<Integer> sleepParameter) throws InterruptedException, SuspendExecution {
-        Fiber.sleep(sleepParameter.orElse(10));
-        return new Saying(ai.incrementAndGet(), name.orElse("name"));
+        Fiber.sleep(sleepParameter.or(10));
+        return new Saying(ai.incrementAndGet(), name.or("name"));
     }
 
     @GET
     @Path("/http")
     @Timed
     public String http(@QueryParam("name") Optional<String> name) throws InterruptedException, SuspendExecution, IOException {
-        return httpClient.execute(new HttpGet("http://localhost:8080/?sleep=10&name=" + name.orElse("name")),
+        return httpClient.execute(new HttpGet("http://localhost:8080/?sleep=10&name=" + name.or("name")),
                                   new BasicResponseHandler());
     }
 
     @GET
     @Path("/fluent")
     @Timed
-    public Integer fluentAPI(
+    public List<String> fluentAPI(
             @QueryParam("id") Optional<Integer> id) throws InterruptedException, SuspendExecution, IOException {
         try (Handle h = jdbi.open()) {
-            h.execute("create table if not exists fluentAPI (id int primary key, name varchar(100))");
-            for (int i = 0; i < 100; i++)
-                h.execute("insert into fluidAPI (id, name) values (?, ?)", i, "stranger " + i);
-            int size = h.createQuery("select name from fluentAPI where id < :id order by id")
-                    .bind("id", id.orElse(50)).map(StringMapper.FIRST).list().size();
-            h.execute("drop table fluentAPI");
-            return size;
+            try {
+                h.execute("create table if not exists fluentAPI (id int primary key, name varchar(100))");
+                for (int i = 0; i < 100; i++)
+                    h.execute("insert into fluentAPI (id, name) values (?, ?)", i, "stranger " + i);
+                return h.createQuery("select name from fluentAPI where id < :id order by id")
+                        .bind("id", id.or(50)).map(StringMapper.FIRST).list();
+            } finally {
+                h.execute("drop table fluentAPI");
+            }
         }
     }
 
     @GET
     @Path("/dao")
     @Timed
-    public String dao(
+    public List<String> dao(
             @QueryParam("id") Optional<Integer> id) throws InterruptedException, SuspendExecution, IOException {
-        dao.createSomethingTable();
-        for (int i = 0; i < 100; i++) dao.insert(i, "name" + i);
-        final String result = dao.findNameById(37);
-        dao.dropSomethingTable();
-        return result;
+        final String result;
+        try {
+            dao.createSomethingTable();
+            for (int i = 0; i < 100; i++) dao.insert(i, "name" + i);
+            result = dao.findNameById(id.or(0));
+        } finally {
+            dao.dropSomethingTable();
+        }
+        return Collections.singletonList(result);
     }
 
     private Object MY_RESOURCE_OBJ = this;
